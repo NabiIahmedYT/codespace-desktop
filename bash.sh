d1fe66f870ee5e4a138c4689c6a47a60e61f52aa
NabiIahmedYT <100964593+NabiIahmedYT@users.noreply.github.com>
2024-04-24 10:24:38 -0400

    Update and rename Dockerfile to bash.sh

--- bash.sh
+++ bash.sh
@@ -1,25 +1,19 @@
-# Use ubuntu as the base image
-FROM ubuntu:latest
-
-# Install necessary packages without prompts
-RUN apt-get update && \
+sudo apt-get update && \
     apt-get install -y --no-install-recommends curl gnupg xfce4 desktop-base dbus-x11 xscreensaver && \
     apt-get clean && \
     rm -rf /var/lib/apt/lists/*
 
-# Add Google Chrome Remote Desktop repository and install
-RUN curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+
+ curl -fsSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - && \
     echo "deb [arch=amd64] https://dl.google.com/linux/chrome-remote-desktop/deb stable main" > /etc/apt/sources.list.d/chrome-remote-desktop.list && \
     apt-get update && \
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends chrome-remote-desktop && \
     apt-get clean && \
-    rm -rf /var/lib/apt/lists/*
+    sudo rm -rf /var/lib/apt/lists/*
 
-# Set Chrome Remote Desktop session command
-RUN echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
+echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session
 
-# Disable display manager
-RUN rm -f /etc/init.d/*tty* && \
+sudo rm -f /etc/init.d/*tty* && \
     echo '#!/bin/sh\nexit 0' > /usr/sbin/policy-rc.d && \
     echo 'manual' > /etc/init/lightdm.override
 
